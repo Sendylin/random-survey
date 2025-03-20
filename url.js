a052d0bc1266a372009c1cc1b941ca409e6e4054
Sendylin <sendylin521@gmail.com>
2025-03-20 23:54:52 +0800

    Update url.js

--- url.js
+++ url.js
@@ -10,7 +10,6 @@
 const urls = [
 	'https://forms.gle/vhfKFDxkrVRX8sXx9',
 	'https://forms.gle/JTxK3vwjSD328GVp8',
-	'https://www.surveycake.com/s/9eDga',
 	'https://www.surveycake.com/s/bgD01',
 	'https://www.surveycake.com/s/0qMp0',
 ];
